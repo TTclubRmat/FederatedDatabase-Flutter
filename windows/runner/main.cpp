93ded120e541d5dfe3a08d989788dcd48d82c97f
wuzhuodao <wuzhuodao25@163.com>
2024-12-16 10:52:09 +0800

    优化WindowsApp屏幕启动位置

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -5,39 +5,43 @@
 #include "flutter_window.h"
 #include "utils.h"
 
+
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
-                      _In_ wchar_t *command_line, _In_ int show_command) {
-  // Attach to console when present (e.g., 'flutter run') or create a
-  // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+        _In_ wchar_t *command_line, _In_ int show_command) {
+    if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
-  }
+    }
 
-  // Initialize COM, so that it is available for use in the library and/or
-  // plugins.
-  ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+    ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
-  flutter::DartProject project(L"data");
+    flutter::DartProject project(L"data");
 
-  std::vector<std::string> command_line_arguments =
-      GetCommandLineArguments();
+    std::vector<std::string> command_line_arguments =
+            GetCommandLineArguments();
 
-  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
+    project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
-  FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(500, 800);
-  if (!window.Create(L"基于gRPC的联邦数据库", origin, size)) {
+    FlutterWindow window(project);
+    Win32Window::Size size(500, 800);
+
+//    // 获取屏幕尺寸
+//    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+//    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    // 计算窗口原点，使其位于屏幕中央
+    Win32Window::Point origin(50,50);
+
+    if (!window.Create(L"基于gRPC的联邦数据库", origin, size)) {
     return EXIT_FAILURE;
-  }
-  window.SetQuitOnClose(true);
+    }
+    window.SetQuitOnClose(true);
 
-  ::MSG msg;
-  while (::GetMessage(&msg, nullptr, 0, 0)) {
+    ::MSG msg;
+    while (::GetMessage(&msg, nullptr, 0, 0)) {
     ::TranslateMessage(&msg);
     ::DispatchMessage(&msg);
-  }
+    }
 
-  ::CoUninitialize();
-  return EXIT_SUCCESS;
+    ::CoUninitialize();
+    return EXIT_SUCCESS;
 }
